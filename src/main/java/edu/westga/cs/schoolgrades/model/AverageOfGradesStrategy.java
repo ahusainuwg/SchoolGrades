bbe7926c139a52c54ee4f65508d8d3d3ddce4254
Ahmed Husain <ahusain1@my.westga.edu>
2019-10-27 21:52:15 -0700

    Commit Completed AverageOfGradesStartegy class in the model package

--- src/main/java/edu/westga/cs/schoolgrades/model/AverageOfGradesStrategy.java
+++ src/main/java/edu/westga/cs/schoolgrades/model/AverageOfGradesStrategy.java
@@ -1,5 +1,18 @@
 package edu.westga.cs.schoolgrades.model;
 
-public class AverageOfGradesStrategy {
+import java.util.List;
+
+public class AverageOfGradesStrategy extends SumOfGradesStrategy{
+	 @Override
+	    public double calculate(final List<Grade> grades) {
+	        if (grades == null) {
+	            throw new IllegalArgumentException("grades list can not be null");
+	        }
+	        if (grades.isEmpty()) {
+	            return 0.0;
+	        }
+	        final double sum = super.calculate(grades);
+	        return sum / grades.size();
+	    }
 
 }
