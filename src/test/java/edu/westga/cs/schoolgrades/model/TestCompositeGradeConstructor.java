22c5ac0ecf5a83b8725aa9a02eb8cdb93c6a135c
Ahmed Husain <ahusain1@my.westga.edu>
2019-12-02 14:32:09 -0800

    Commit updated test class TestCompositeGradeConstructor with Mockito Mocking

--- src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeConstructor.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestCompositeGradeConstructor.java
@@ -3,10 +3,13 @@ package edu.westga.cs.schoolgrades.model;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+
 public class TestCompositeGradeConstructor {
 
-	
-	
 	@Test
 	public void shouldNotAllowNullStrategy() {
 		assertThrows(IllegalArgumentException.class, () ->{ 
@@ -16,7 +19,7 @@ public class TestCompositeGradeConstructor {
 	
 	@Test
 	public void shouldHaveNoGradesWhenCreated() {
-		CompositeGrade grade = new CompositeGrade(new SumOfGradesStrategy());
+		CompositeGrade grade = new CompositeGrade(mock(SumOfGradesStrategy.class));
 		assertTrue(grade.getGrades().isEmpty());
 	}
 
